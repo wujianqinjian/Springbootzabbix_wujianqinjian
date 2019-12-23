1bc19bb0a765708e748539b34a2f222829b290ec
wujianqinjian@163.COM <wujianqinjian@163.com>
2019-12-23 18:01:53 +0800

    添加logger相关信息！

--- src/main/java/wechat/controller/HelloWeChat.java
+++ src/main/java/wechat/controller/HelloWeChat.java
@@ -1,10 +1,14 @@
 package wechat.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import wechat.bean.WeChat;
+
+import java.lang.reflect.Method;
 
 @RestController
 public class HelloWeChat {
@@ -26,7 +30,13 @@ public class HelloWeChat {
         return wxtestmessage;
     }*/
     @RequestMapping("/helloWechat")
-    public String sayHello() {
+    public String sayHello() throws Exception {
+        Logger logger=LoggerFactory.getLogger(getClass());
+        /*logger.info((String) weChat.getClass().getMethod("toString",String.class).invoke(weChat));
+         */
+        System.out.println("-----------------------------------------------------");
+        System.out.println(weChat.getClass().getMethod("toString").invoke(weChat));
+        System.out.println("-----------------------------------------------------");
         return "您好" +"\n"+ wechatName+"\n" + "欢迎你"+ "\n"+weChat;
     }
 
