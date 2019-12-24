b402ce728871dc1bf3f450e41db14c717d02bb3e
wujianqinjian@163.COM <wujianqinjian@163.com>
2019-12-24 17:59:04 +0800

    增加了登录页面

--- src/main/java/traditional/controller/HelloWujianqinjian.java
+++ src/main/java/traditional/controller/HelloWujianqinjian.java
@@ -2,6 +2,7 @@ package traditional.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import traditional.bean.Person;
 
@@ -14,5 +15,17 @@ public class HelloWujianqinjian {
     @RequestMapping("/helloPersonWujianqinjian")
     public String helloWujianqinjian(){
         return "你好"+person;
+    }
+
+    @ResponseBody
+    @RequestMapping("/index")
+    public String indexVue(){
+        return "index.html";
+            }
+
+    @ResponseBody
+    @RequestMapping("/login")
+    public String loginVue(){
+        return "login.html";
     }
 }
